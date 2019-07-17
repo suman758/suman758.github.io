cb0bae29060bb1c2c4a3ec0b53239d443421dc75
suman@wrctpl.com <grass6=!>
2019-07-18 05:04:44 +0530

    commit
    commit

--- main-es2015.js
+++ main-es2015.js
@@ -389,7 +389,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 const environment = {
     production: false,
-    baseUrl: 'http://aqueous-river-43130.herokuapp.com/'
+    baseUrl: 'https://aqueous-river-43130.herokuapp.com/'
 };
 /*
  * For easier debugging in development mode, you can import the following file
