bd549f359ae2ddfe766f6bf2ef3f8ce02bf42aba
suman@wrctpl.com <grass6=!>
2019-07-18 04:47:28 +0530

    commit by suman
    commit by suman

--- main-es5.js
+++ main-es5.js
@@ -402,7 +402,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 var environment = {
     production: false,
-    baseUrl: 'http://localhost:1337/localhost:3000/'
+    baseUrl: 'https://aqueous-river-43130.herokuapp.com/'
 };
 /*
  * For easier debugging in development mode, you can import the following file
